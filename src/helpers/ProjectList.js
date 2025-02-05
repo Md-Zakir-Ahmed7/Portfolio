d10d6bf86a00b543da651f6ffeb66859c0cc9e5d
zak710 <zakir.ah@rivan.in>
2025-02-05 16:51:24 +0530

    new changes

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,6 +1,7 @@
 import SUPERSTORE from "../assets/Superstore_Report.png";
 import Reliance from "../assets/Reliance_Report.png";
 import Excel from "../assets/Excel_Report.png";
+import Churn from "../assets/Churn_Analysis.png";
 
 export const ProjectList = [
   {
@@ -9,10 +10,16 @@ export const ProjectList = [
     skills: "PowerBI,Power Query,DAX,Data Extraction,Data Modelling,ETL",
   },
   {
+    name: "CHURN ANALYSIS",
+    image: Churn,
+    skills: "PowerBI,Power Query,DAX,Data Extraction,Data Modelling,ETL",
+  },
+  {
     name: "RELIANCE ANALYSIS",
     image: Reliance,
     skills: "PowerBI,Power Query,DAX,Data Extraction,Data Modelling,ETL",
   },
+
   {
     name: "EXCEL REPORT",
     image: Excel,
